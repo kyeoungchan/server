76e50a8b45e7ced72c130026e18399f0fe1f35be
charles <wkc1002@naver.com>
2023-08-24 15:08:02 +0900

    Register Spring Container

--- src/main/java/hello/spring/HelloController.java
+++ src/main/java/hello/spring/HelloController.java
@@ -1,2 +1,14 @@
-package hello.spring;public class HelloController {
+package hello.spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-spring")
+    public String hello() {
+        System.out.println("HelloController.hello");
+        return "hello spring!";
+    }
 }
